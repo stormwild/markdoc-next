ae66f2972be003f4e77aebeb93abe1f139e72447
Alexander R. Torrijos <artorrijos@gmail.com>
2022-05-30 22:02:53 +0800

    Updates

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth'
+import OktaProvider from 'next-auth/providers/okta'
+
+const options = {
+  // Configure one or more authentication providers
+  providers: [
+    OktaProvider({
+      clientId: process.env.OKTA_CLIENT_ID || '',
+      clientSecret: process.env.OKTA_CLIENT_SECRET || '',
+      issuer: process.env.OKTA_ISSUER || '',
+    }),
+    // ...add more providers here
+  ],
+}
+
+export default NextAuth(options)
